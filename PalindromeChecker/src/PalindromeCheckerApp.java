e7fa1c7b1f763d8907ef13bba7f1e77493eac0a2
Ishita12222 <ia3170@srmist.edu.in>
2026-02-27 10:22:18 +0530

    Ishita  Added UC-12

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,37 +1,76 @@
-// File: PalindromeCheckerApp.java
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+interface PalindromeStrategy {
+    boolean isPalindrome(String text);
+}
+
+class StackStrategy implements PalindromeStrategy {
+
+    @Override
+    public boolean isPalindrome(String text) {
+        Stack<Character> stack = new Stack<>();
+        String normalized = text.replaceAll("\\s+", "").toLowerCase();
+
+        for (char ch : normalized.toCharArray()) {
+            stack.push(ch);
+        }
+
+        for (char ch : normalized.toCharArray()) {
+            if (ch != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+
+    @Override
+    public boolean isPalindrome(String text) {
+        Deque<Character> deque = new ArrayDeque<>();
+        String normalized = text.replaceAll("\\s+", "").toLowerCase();
+
+        for (char ch : normalized.toCharArray()) {
+            deque.addLast(ch);
+        }
+
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
-        PalindromeChecker checker = new PalindromeChecker("Racecar");
-        if (checker.checkPalindrome()) {
+    private PalindromeStrategy strategy;
+
+    public PalindromeCheckerApp(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void check(String text) {
+        if (strategy.isPalindrome(text)) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not a palindrome");
         }
     }
-}
 
-// Non-public class in same file
-class PalindromeChecker {
+    public static void main(String[] args) {
+        String text = "Racecar";
 
-    private String text;
+        // Using Stack strategy
+        PalindromeCheckerApp app1 = new PalindromeCheckerApp(new StackStrategy());
+        app1.check(text);
 
-    public PalindromeChecker(String text) {
-        this.text = text;
-    }
-
-    public boolean checkPalindrome() {
-        String normalized = text.replaceAll("\\s+", "").toLowerCase();
-        int start = 0;
-        int end = normalized.length() - 1;
-
-        while (start < end) {
-            if (normalized.charAt(start) != normalized.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
+        // Using Deque strategy
+        PalindromeCheckerApp app2 = new PalindromeCheckerApp(new DequeStrategy());
+        app2.check(text);
     }
 }
