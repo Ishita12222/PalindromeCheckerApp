5950099b951dc655a22f4f010b720c9df11f6697
Ishita12222 <ia3170@srmist.edu.in>
2026-02-27 09:01:21 +0530

    [Ishita] Added [UC1]

---
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println("     Welcome to PalindromeChecker  ");
+        System.out.println("===================================");
+        System.out.println("Application Name: PalindromeChecker App");
+        System.out.println("Version: 1.0.0");
+        System.out.println("Developed for palindrome validation.");
+        System.out.println("-----------------------------------");
+        System.out.println("Application started successfully!");
+        System.out.println("-----------------------------------");
+    }
+}
