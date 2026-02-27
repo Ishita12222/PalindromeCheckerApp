c2c86f65b20c1dcf73631f9d55f55d5b101fa13c
Ishita12222 <ia3170@srmist.edu.in>
2026-02-27 10:15:19 +0530

    Ishita  Added UC-10

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -2,24 +2,26 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String text = "level";
+        String text = "A man a plan a canal Panama";
+        String normalized = text.replaceAll("\\s+", "").toLowerCase();
 
-        if (isPalindrome(text, 0, text.length() - 1)) {
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not a palindrome");
         }
-    }
-
-    public static boolean isPalindrome(String str, int start, int end) {
-        if (start >= end) {
-            return true;
-        }
-
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-
-        return isPalindrome(str, start + 1, end - 1);
     }
 }
